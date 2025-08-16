5b88d4abff6199e9cdca9a49941cc3a1cd3033b6
Adefolarin.. <suberufolarin@gmail.com>
2025-08-16 19:34:15 +0100

    Update datatypes.js

--- datatypes.js
+++ datatypes.js
@@ -89,10 +89,124 @@ let legalAge = 18
 let bolaAge = 40;
 let bimpeAge = "40";
 
+// console.log( bolaAge == bimpeAge)
+// console.log( bolaAge === bimpeAge)
 
-console.log( bolaAge == bimpeAge)
+// greater than (>) less than (<) , greater or equals to (>=) , less than or equals to (<=) 
 
-console.log( bolaAge === bimpeAge)
+const LEGALAGE = 18;
+
+bimpeAge = 18;
+
+
+
+// console.log('Can bimple vote? ', bimpeAge >= LEGALAGE)
+
+// LOGIC GATE (AND, OR , NOT)
+
+// AND GATE (&&)
+
+// z will be true , if both x and y is true
+
+
+// console.log("------ AND OPERATOR --------")
+
+
+let x = true
+let y = true
+let a = false
+let b = false
+
+
+
+// console.log(true && true)
+// console.log(true && false)
+// console.log(false && false)
+
+// console.log('z = ', x && y)
+// console.log('z = ', x && a)
+// console.log('z = ', a && b)
+
+
+
+// console.log('z = ', bimpeAge >= LEGALAGE && bolaAge >= LEGALAGE && true && false && true && true)
+
+
+
+
+
+// OR GATE (||)
+
+// Z will be true , if any of x and y is true
+
+// console.log("------ OR OPERATOR --------")
+
+
+// console.log(true || true)
+// console.log(true || false)
+// console.log(false || false)
+
+// console.log(true || false || false || false)
+
+// NOT GATE (!)
+
+// it negates a boolean value
+
+
+console.log("------ NOT OPERATOR --------")
+
+// console.log(true)
+// console.log(false)
+// console.log(!true)
+// console.log(!false)
+
+
+// console.log(5 > 2)
+
+// console.log(! 5>2)
+
+// console.log(!! 5>2)
+
+// console.log((! 5 > 2))
+
+
+// console.log(! (! 5 > 2))
+// console.log(!! 5>2)
+// console.log(-4 * -2)
+
+
+
+// Reference data type
+
+// 1) Arrays 2) Objects
+
+
+let agesArray = [6 , 7, 9 , 10 , 10, 345, 74748, 'ada', 'bola', 'bimpe', false, true,]
+
+let folaObj = {
+    name : 'Adefolarin',
+    age : 30,
+    sex : 'male',
+    favColor: 'red',
+    club : 'lIVERPOOL'
+}
+
+
+// almost everything in javascript is an object!!!!
+
+// console.log('ages array ', agesArray)
+
+
+let fruitArray = ['apple', 'orange', 'banana', 'pear', 'coconut']
+
+console.log('items in this array ', fruitArray)
+fruitArray.pop()
+fruitArray.shift()
+fruitArray.push("Strawberry")
+fruitArray.unshift("Papaya")
+console.log('items in this array ', fruitArray)
+
+
 
 
 
